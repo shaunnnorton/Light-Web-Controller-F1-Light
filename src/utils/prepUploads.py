471e7dfac3d3e22cee94c98b2b6540c32366960a
Shaun Norton <shaunnorton87@gmail.com>
2025-12-30 14:37:11 -0800

    debugging

--- src/utils/prepUploads.py
+++ src/utils/prepUploads.py
@@ -5,29 +5,29 @@ from .. import config
 
 def parseUploadedExcel(filename):
 
-    try:
-        export_json = {}
-        uploadedDF = pd.read_excel(filename,engine="openpyxl").to_dict(orient="index")
-        for row in uploadedDF:
-            #print(type(uploadedDF[row]["Date"]))
-            export_json[uploadedDF[row]["Date"].strftime("%-m/%-d/%Y")] = {
-                "Track Name": uploadedDF[row]["Track Name"],
-                "RaceNumber": uploadedDF[row]["Race Number"]
-            }
-        
-        with open(f"./src/static/schedules/ImportedSchedule{dt.now().strftime("%Y%m%d%H%M%s")}.json", "w") as newfile:
-            newfile.write(json.dumps(export_json))
-            new_filename = newfile.name
+    # try:
+    export_json = {}
+    uploadedDF = pd.read_excel(filename,engine="openpyxl").to_dict(orient="index")
+    for row in uploadedDF:
+        #print(type(uploadedDF[row]["Date"]))
+        export_json[uploadedDF[row]["Date"].strftime("%-m/%-d/%Y")] = {
+            "Track Name": uploadedDF[row]["Track Name"],
+            "RaceNumber": uploadedDF[row]["Race Number"]
+        }
+    
+    with open(f"./src/static/schedules/ImportedSchedule{dt.now().strftime("%Y%m%d%H%M%s")}.json", "w") as newfile:
+        newfile.write(json.dumps(export_json))
+        new_filename = newfile.name
 
-        appconfig = config.Config()
+    appconfig = config.Config()
 
-        with open("attributes.json","w+") as config_file:
-            config_json = json.load(config_file)
-            config_json.currentScheduleFile = "./src/static/schedules/"+new_filename
-            print(config_json)
+    with open("attributes.json","w+") as config_file:
+        config_json = json.load(config_file)
+        config_json.currentScheduleFile = "./src/static/schedules/"+new_filename
+        print(config_json)
 
-            config_file.write(json.dumps(config_json))
+        config_file.write(json.dumps(config_json))
 
-        return "File successfully uploaded!"
-    except Exception as e:
-        return f"Error when uploading file. Please check format and try again. \n EXCEPTION {e}"
+    return "File successfully uploaded!"
+    # except Exception as e:
+    #     return f"Error when uploading file. Please check format and try again. \n EXCEPTION {e}"
