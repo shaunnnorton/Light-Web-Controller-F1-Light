b8c326f19b2cfd5f09a6235c5ad0d825ec1d9100
Shaun Norton <shaunnorton87@gmail.com>
2025-12-30 13:50:45 -0800

    Debugging

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,7 @@ import os
 # APPSETUP
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "THIS IS NOT A SECRET"
 
 from src.main import main_routes as main_routes
 app.register_blueprint(main_routes)
