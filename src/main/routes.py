935f162eab306a703e3869a03c1c07c4d9028714
Shaun Norton <shaunnorton87@gmail.com>
2025-12-30 13:25:48 -0800

    Adding ability to upload file

--- src/main/routes.py
+++ src/main/routes.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request, jsonify, Blueprint
+from flask import Flask, render_template, request, jsonify, Blueprint, flash,redirect, url_for
+import os
 import time
 import board
 import neopixel
 import random
+from src.utils import prepUploads as pu
 from .. import config
 
 
@@ -17,7 +19,7 @@ print(appconfig.pixel_order)
 num_pixels = appconfig.pixelcount
 ORDER = appconfig.pixel_order
 
-
+UPLOAD_FOLDER = "/src/static/schedules"
 
 
 pixels = neopixel.NeoPixel(board.D18, num_pixels, brightness=1, auto_write=False, pixel_order=ORDER)
@@ -77,7 +79,7 @@ def AutomaticUpdate():
 
     row = request.args.get("row")
     col = request.args.get("column")
-    
+
     background_color = (255,255,255)
     color = (255,0,0)
     pixels.fill(background_color)
@@ -91,3 +93,37 @@ def AutomaticUpdate():
     return jsonify({"message":"color recived"})
 
 
+def allowed_file_upload(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ("xlsx", "xls")
+
+@main.route('/UploadNewFile', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file_upload(file.filename):
+            file.save(os.path.join(UPLOAD_FOLDER, file.filename))
+
+            uploadparse = pu.parseUploadedExcel(os.path.join(UPLOAD_FOLDER, file.filename))
+
+
+            flash(uploadparse)
+            return redirect(url_for('index'))
+    return '''
+    <!doctype html>
+    <title>Upload new Schedule File</title>
+    <h1>Upload new Schedule File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
---
+++ src/utils/prepUploads.py
@@ -0,0 +1,30 @@
+import json
+import pandas as pd
+from datetime import datetime as dt
+from .. import config
+
+def parseUploadedExcel(filename):
+
+    try:
+        export_json = {}
+        uploadedDF = pd.read_excel(filename,engine="openpyxl").to_dict(orient="index")
+        for row in uploadedDF:
+            export_json[row["Date"]] = {
+                "Track Name": row["Track Name"],
+                "RaceNumber": row["Race Number"]
+            }
+        
+        with open(f"./src/static/schedules/ImportedSchedule{dt.now().strftime("%Y%m%d%H%M%s")}.json", "w") as newfile:
+            newfile.write(json.dumps(export_json))
+            new_filename = newfile.name
+
+        appconfig = config.Config()
+
+        with open(config.currentScheduleFile,"rw") as config_file:
+            config_json = json.load(json)
+            config_json.currentScheduleFile = new_filename
+            config_file.write(json.dump(config_json))
+
+        return "File successfully uploaded!"
+    except:
+        return "Error when uploading file. Please check format and try again."
