e0c097b6db8f734b537d3170d7e3944de9b8dba0
Shaun Norton <shaunnorton87@gmail.com>
2025-12-30 11:55:03 -0800

    debugging

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from src.utils import cal
 if __name__ == "__main__":
 
     schedule = cal.Schedule(".src/static/schedules/testSchedule.json")
-    threading.Thread(target=schedule.functionalLoop)
-    threading.Thread(target=app.run, kwargs={"debug":False,"port":8080, "host":"0.0.0.0"})
+    background_thread = threading.Thread(target=schedule.functionalLoop)
+    server_thread = threading.Thread(target=app.run, kwargs={"debug":False,"port":8080, "host":"0.0.0.0"})
     #app.run(debug=False,port=8080, host="0.0.0.0")
+    background_thread.start()
+    server_thread.start()
